55c0a77ee839658aef431702add1302393367c4d
Cyp[Lee Byoung Gyu] <cypher9715@naver.com>
2021-11-13 23:28:24 +0900

    update cryptoPP vs_2022 version, compile debug info Zi -> C7

--- cypReference/src/cpp/test.cpp
+++ cypReference/src/cpp/test.cpp
@@ -1,10 +1,6 @@
-﻿#include "cyp_hash.hpp"
+﻿#include "cyp_communication.hpp"
 
 int main()
 {
-	cyp::hash::Pbkdf2 pbkdf2;
-	cyp::hash::Hkdf hkdf;
-
-	std::cout << pbkdf2.strToPbkdf2<CryptoPP::SHA256>("123123", "sss", 1024) << std::endl;
-	std::cout << hkdf.strToHkdf<CryptoPP::SHA256>("password", "salt", "HKDF key derivation") << std::endl;
+	
 }
