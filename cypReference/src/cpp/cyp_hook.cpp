95c09d2863105c011f9e0f318e9441be632b0305
Cyp <Cypher9715@naver.com>
2022-05-10 06:01:14 +0900

    update cyp::hook::Hook_KeyBoard

--- cypReference/src/cpp/cyp_hook.cpp
+++ cypReference/src/cpp/cyp_hook.cpp
@@ -3,11 +3,12 @@
 namespace cyp
 {
 	namespace hook
-	{ 
-		HHOOK _hook;
-		KBDLLHOOKSTRUCT _kbdStruct;
-
-		LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+	{
+		HHOOK cyp::hook::Hook_KeyBoard::_hook;
+		KBDLLHOOKSTRUCT cyp::hook::Hook_KeyBoard::_kbdStruct;
+		bool cyp::hook::Hook_KeyBoard::existHook;
+		
+		LRESULT __stdcall Hook_KeyBoard::HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 		{
 			if (nCode >= 0)
 			{
@@ -15,27 +16,57 @@ namespace cyp
 				{
 					_kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 
-					if (GetAsyncKeyState(VK_CONTROL) & 0x8000 && _kbdStruct.vkCode == VK_F2)
+					switch (_kbdStruct.vkCode)
 					{
+					case VK_F2:
+					{
+						if (!PRESSING_CONTROL())
+							goto nextHook;
+						
 						MessageBox(NULL, "Ctrl + F2 is pressed!", "key pressed", MB_ICONINFORMATION);
+						break;
+					}
+
+					case VK_F1:
+					{
+						MessageBox(NULL, "F1 is pressed!", "key pressed", MB_ICONINFORMATION);
+					}
 					}
 				}
-
 			}
+
+			nextHook:
 
 			return CallNextHookEx(_hook, nCode, wParam, lParam);
 		}
 
-		void Hook_KeyBoard::SetHook()
+		SHORT Hook_KeyBoard::PRESSING_CONTROL()
 		{
-			if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
-			{
-				MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
-			}
+			return GetAsyncKeyState(VK_CONTROL) & 0x8000;
+		}
+
+		SHORT Hook_KeyBoard::PRESSING_SHIFT()
+		{
+			return GetAsyncKeyState(VK_SHIFT) & 0x8000;
+		}
+
+		bool Hook_KeyBoard::SetHook()
+		{
+			if (existHook == true) 
+				return false;
+
+			_hook = SetWindowsHookEx(WH_KEYBOARD_LL, Hook_KeyBoard::HookCallback, NULL, 0);
+
+			if (_hook == NULL)
+				return false;
+
+			existHook = true;
+			return true;
 		}
 
 		void Hook_KeyBoard::RemoveHook()
 		{
+			existHook = false;
 			UnhookWindowsHookEx(_hook);
 		}
 	}
--- cypReference/src/hpp/cyp_hook.hpp
+++ cypReference/src/hpp/cyp_hook.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "cyp_common.hpp"
 #include <Windows.h>
 
 namespace cyp
@@ -8,10 +9,17 @@ namespace cyp
 	{
 		class Hook_KeyBoard
 		{
+			static bool existHook;	// Prevent duplicate hooks created.
+			static HHOOK _hook;
+			static KBDLLHOOKSTRUCT _kbdStruct;
+
 		private:
+			LRESULT static __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam);
+			SHORT static PRESSING_CONTROL();
+			SHORT static PRESSING_SHIFT();
 
 		public:
-			void SetHook();
+			bool SetHook();
 			void RemoveHook();
 		};
 
@@ -20,6 +28,9 @@ namespace cyp
 		private:
 
 		public:
+
 		};
+
+
 	}
 }
